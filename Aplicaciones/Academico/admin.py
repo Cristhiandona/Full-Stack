cf89dfc663847cdf1a261a688b6a120cf4525e3e
cristhiandona <cristhiandona@gmail.com>
2024-03-20 23:55:19 -0500

    Clase de Full Stack

---
+++ Aplicaciones/Academico/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Usuario
+
+# Registro el modelo.
+
+admin.site.register(Usuario)
---
+++ Aplicaciones/Academico/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    PK = models.CharField(primary_key=True, max_lenght=6)
+    nombre = models.CharField(max_lenght=60)
+    apellido = models.CharField(max_lenght=60)
