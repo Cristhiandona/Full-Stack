38a81e87ce1d808f3766210625eb74a1219e3f5b
cristhiandona <cristhiandona@gmail.com>
2024-03-21 00:00:37 -0500

    Se agrega modificacion en Models

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -6,3 +6,5 @@ class Usuario(models.Model):
     PK = models.CharField(primary_key=True, max_lenght=6)
     nombre = models.CharField(max_lenght=60)
     apellido = models.CharField(max_lenght=60)
+
+    #Comentarios para GIT
